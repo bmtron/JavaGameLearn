208f8d02d8713f1ca835b508dfc99e40aefe8122
Brendan M Meehan <bmmeehan3@gmail.com>
2021-02-14 02:37:30 -0500

    Initial commit

---
+++ src/com/game/main/Player.java
@@ -0,0 +1,24 @@
+package com.game.main;
+
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+
+        velX = 1;
+    }
+
+    @Override
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(x, y, 1, 1);
+    }
+}
